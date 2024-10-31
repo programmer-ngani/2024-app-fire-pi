993ced178fce8c4e5df45199f2686e2a14614c24
programmer-ngani <chingudachi00@gmail.com>
2024-11-01 06:52:58 +0800

    Create copy-paste.py

---
+++ tag-notes/copy-paste.py
@@ -0,0 +1,26 @@
+Mga Hakbang:
+
+A. Pagbukas ng Thonny IDE at Pag-connect ng Pico
+1) Buksan ang Thonny IDE.
+2) Mula sa Menu Bar: pillin ang Tools, Options, Interpreter.
+3) Piliin ang Local Python 3 na interpreter ng code (kung walang Pico).
+4) Isaksak sa USB Port ang Pico.
+5) Pumunta ulit sa Menu Bar: pillin ang Tools, Options, Interpreter.
+6) Piliin ang MicroPython (Raspberry Pi Pico) na interpreter ng code.
+7) Piliin ang Try to detect port automatically sa Port or WebREPL.
+8) Shortcut: Select Interpreter sa bottom-right corner ng Thonny IDE window.
+8.1) Select or re-select MicroPython (Raspberry Pi Pico) na backend.
+8.2) Walang error na lalabas sa Shell sa baba ng Thonny IDE window.
+
+B. Paggawa ng Python Script (Code) sa Thonny IDE at Pagsave ng File sa Pico
+1) Magtype ng code sa editor.
+print("testing lang.")
+2) Para mag-testing ng code.
+2.1) Mula sa Menu Bar: pillin ang Run, Run current script (F5).
+2.2) Shortcut: pindutin ang Run Icon.
+3) Para mag-save ng code.
+3.1) Mula sa Menu Bar: pillin ang File, Save o Save As.
+3.2) Shortcut: pindutin ang CTRL+S ng sabay.
+3.3) Where to save to? Piliin ang Raspberry Pi Pico.
+3.4) Mag-type ng pangalan ng file (test-print.py), click OK.
+3.5) Pindutin ang CTRL+W ng sabay para mag-close ang editor.
